de31f8cd7abdf845374c8a6a4ee1c6e8c157f883
chinneii <chincy02@gmail.com>
2023-08-10 08:42:34 +0800

    milestone 2

--- milestone2/operate.py
+++ milestone2/operate.py
@@ -1,4 +1,4 @@
-# teleoperate the robot with your keyboard (M1)
+# teleoperate the robot and perform SLAM
 # will be extended in following milestones for system integration
 
 # basic python packages
@@ -15,6 +15,12 @@ import util.measure as measure # measurements
 import pygame # python package for GUI
 import shutil # python package for file operations
 
+# import SLAM components
+sys.path.insert(0, "{}/slam".format(os.getcwd()))
+from slam.ekf import EKF
+from slam.robot import Robot
+import slam.aruco_detector as aruco
+
 
 class Operate:
     def __init__(self, args):
@@ -30,6 +36,12 @@ class Operate:
             self.pibot = dh.DatasetPlayer("record")
         else:
             self.pibot = Alphabot(args.ip, args.port)
+
+        # initialise SLAM parameters
+        self.ekf = self.init_ekf(args.calib_dir, args.ip)
+        self.aruco_det = aruco.aruco_detector(
+            self.ekf.robot, marker_length = 0.06) # size of the ARUCO markers
+
         if args.save_data:
             self.data = dh.DatasetWriter('record')
         else:
@@ -47,11 +59,14 @@ class Operate:
         self.ekf_on = False
         self.double_reset_comfirm = 0
         self.image_id = 0
-        self.notification = 'Teleoperate the robot'
+        self.notification = 'Press ENTER to start SLAM'
         # a 5min timer
         self.count_down = 300
         self.start_time = time.time()
         self.control_clock = time.time()
+        # initialise images
+        self.img = np.zeros([240,320,3], dtype=np.uint8)
+        self.aruco_img = np.zeros([240,320,3], dtype=np.uint8)
         self.bg = pygame.image.load('pics/gui_mask.jpg')
 
     # wheel control
@@ -67,12 +82,28 @@ class Operate:
         drive_meas = measure.Drive(lv, rv, dt)
         self.control_clock = time.time()
         return drive_meas
-
     # camera control
     def take_pic(self):
         self.img = self.pibot.get_image()
         if not self.data is None:
             self.data.write_image(self.img)
+
+    # SLAM with ARUCO markers       
+    def update_slam(self, drive_meas):
+        lms, self.aruco_img = self.aruco_det.detect_marker_positions(self.img)
+        if self.request_recover_robot:
+            is_success = self.ekf.recover_from_pause(lms)
+            if is_success:
+                self.notification = 'Robot pose is successfuly recovered'
+                self.ekf_on = True
+            else:
+                self.notification = 'Recover failed, need >2 landmarks!'
+                self.ekf_on = False
+            self.request_recover_robot = False
+        elif self.ekf_on: # and not self.debug_flag:
+            self.ekf.predict(drive_meas)
+            self.ekf.add_landmarks(lms)
+            self.ekf.update(lms)
 
     # save images taken by the camera
     def save_image(self):
@@ -85,20 +116,46 @@ class Operate:
             self.command['save_image'] = False
             self.notification = f'{f_} is saved'
 
+    # wheel and camera calibration for SLAM
+    def init_ekf(self, datadir, ip):
+        fileK = "{}intrinsic.txt".format(datadir)
+        camera_matrix = np.loadtxt(fileK, delimiter=',')
+        fileD = "{}distCoeffs.txt".format(datadir)
+        dist_coeffs = np.loadtxt(fileD, delimiter=',')
+        fileS = "{}scale.txt".format(datadir)
+        scale = np.loadtxt(fileS, delimiter=',')
+        if ip == 'localhost':
+            scale /= 2
+        fileB = "{}baseline.txt".format(datadir)  
+        baseline = np.loadtxt(fileB, delimiter=',')
+        robot = Robot(baseline, scale, camera_matrix, dist_coeffs)
+        return EKF(robot)
+
+    # save SLAM map
+    def record_data(self):
+        if self.command['output']:
+            self.output.write_map(self.ekf)
+            self.notification = 'Map is saved'
+            self.command['output'] = False
+
     # paint the GUI            
     def draw(self, canvas):
         canvas.blit(self.bg, (0, 0))
         text_colour = (220, 220, 220)
         v_pad = 40
         h_pad = 20
-        # monitor the camera view
-        self.img = self.pibot.get_image()
-        robot_view = cv2.resize(self.img, (320, 240))
+
+        # paint SLAM outputs
+        ekf_view = self.ekf.draw_slam_state(res=(320, 480+v_pad),
+            not_pause = self.ekf_on)
+        canvas.blit(ekf_view, (2*h_pad+320, v_pad))
+        robot_view = cv2.resize(self.aruco_img, (320, 240))
         self.draw_pygame_window(canvas, robot_view, 
                                 position=(h_pad, v_pad)
                                 )
-        # space to add M2 and M3 monitor views
-        self.put_caption(canvas, caption='SLAM (M2)', position=(2*h_pad+320, v_pad)) # M2
+
+        # canvas.blit(self.gui_mask, (0, 0))
+        self.put_caption(canvas, caption='SLAM', position=(2*h_pad+320, v_pad)) # M2
         self.put_caption(canvas, caption='Detector (M3)',
                          position=(h_pad, 240+2*v_pad)) # M3
         self.put_caption(canvas, caption='PiBot Cam', position=(h_pad, v_pad))
@@ -134,21 +191,19 @@ class Operate:
     # keyboard teleoperation        
     def update_keyboard(self):
         for event in pygame.event.get():
-            ############### add your codes below ###############
+            ########### replace with your M1 codes ###########
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                
-                self.command['motion'] = [1.5, 0]
+                pass # TODO: replace with your M1 code to make the robot drive forward
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                
-                self.command['motion'] = [-1.5, 0.05]
+                pass # TODO: replace with your M1 code to make the robot drive backward
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                self.command['motion'] = [0, 5]
+                pass # TODO: replace with your M1 code to make the robot turn left
             # drive right
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                self.command['motion'] = [0, -5]
+                pass # TODO: replace with your M1 code to make the robot turn right
             ####################################################
             # stop
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
@@ -156,10 +211,41 @@ class Operate:
             # save image
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_i:
                 self.command['save_image'] = True
+            # save SLAM map
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                self.command['output'] = True
+            # reset SLAM map
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                if self.double_reset_comfirm == 0:
+                    self.notification = 'Press again to confirm CLEAR MAP'
+                    self.double_reset_comfirm +=1
+                elif self.double_reset_comfirm == 1:
+                    self.notification = 'SLAM Map is cleared'
+                    self.double_reset_comfirm = 0
+                    self.ekf.reset()
+            # run SLAM
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                n_observed_markers = len(self.ekf.taglist)
+                if n_observed_markers == 0:
+                    if not self.ekf_on:
+                        self.notification = 'SLAM is running'
+                        self.ekf_on = True
+                    else:
+                        self.notification = '> 2 landmarks is required for pausing'
+                elif n_observed_markers < 3:
+                    self.notification = '> 2 landmarks is required for pausing'
+                else:
+                    if not self.ekf_on:
+                        self.request_recover_robot = True
+                    self.ekf_on = not self.ekf_on
+                    if self.ekf_on:
+                        self.notification = 'SLAM is running'
+                    else:
+                        self.notification = 'SLAM is paused'
             # quit
             elif event.type == pygame.QUIT:
                 self.quit = True
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: 
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.quit = True
         if self.quit:
             pygame.quit()
@@ -171,7 +257,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", metavar='', type=str, default='localhost')
-    parser.add_argument("--port", metavar='', type=int, default=40000)
+    parser.add_argument("--port", metavar='', type=int, default=8000)
+    parser.add_argument("--calib_dir", type=str, default="calibration/param/")
     parser.add_argument("--save_data", action='store_true')
     parser.add_argument("--play_data", action='store_true')
     args, _ = parser.parse_known_args()
@@ -213,11 +300,9 @@ if __name__ == "__main__":
         operate.update_keyboard()
         operate.take_pic()
         drive_meas = operate.control()
+        operate.update_slam(drive_meas)
+        operate.record_data()
         operate.save_image()
         # visualise
         operate.draw(canvas)
         pygame.display.update()
-
-
-
-
